3a3cb4a37daf67ee3940c963046440e0741cdb23
suhokim <suhokim0501@snu.ac.kr>
2024-06-07 10:57:06 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
+    int start = cnt == 0 ? 1 : vec[cnt - 1] + 1;
 
-
-
-
-
-
+    for (int i = start; i <= N; ++i) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
+    return 0;
 }
